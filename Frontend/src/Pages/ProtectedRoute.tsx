de3d426f6f04445a1cc8134b3ba870abdb109265
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-06-08 13:12:20 +0530

    Updated the Routes

--- Frontend/src/Pages/ProtectedRoute.tsx
+++ Frontend/src/Pages/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet} from "react-router-dom";
 
 function ProtectedRoute() {
   const code : string | null = window.localStorage.getItem("code");
-  return <div>{ code ? <Outlet/> : <Navigate to={'/login'}/>}</div>;
+  return <div>{ code ? <Outlet/> : <Navigate to={'/login/home'}/>}</div>;
 }
 
 export default ProtectedRoute;
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -21,6 +21,9 @@ import Login from "./Pages/Login.tsx";
 import { MainRoute } from "./Pages/MainRoute.tsx";
 import { ServerlistPage } from "./Pages/ServerlistPage.tsx";
 import LeaveMessage from "./Pages/LeaveMessage.tsx";
+import { AboutPage } from "./Pages/Landing page/AboutPage.tsx";
+import { ContactPage } from "./Pages/Landing page/ContactPage.tsx";
+import { Homepage } from "./Pages/Landing page/Homepage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -109,6 +112,18 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+    children:[
+      {
+        path:'home',
+        element:<Homepage/>
+      },{
+        path:'about-us',
+        element:<AboutPage/>
+      },{
+        path:'contact-us',
+        element:<ContactPage/>
+      }
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
