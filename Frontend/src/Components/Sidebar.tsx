e0d0359d2e0756a55e31cdef8ac93b2718a47859
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-06-13 20:21:32 +0530

    Updated the Sidebar UI

--- Frontend/src/Components/Sidebar.tsx
+++ Frontend/src/Components/Sidebar.tsx
@@ -1,13 +1,26 @@
-import Subsidebar from "./Subsidebar"
-import {DataSidebar} from "../Resources/Data"
+import Subsidebar from "./Subsidebar";
+import { DataSidebar } from "../Resources/Data";
+import { useLocation } from "react-router-dom";
 
 function Sidebar() {
+  const { pathname } = useLocation();
   return (
     <div className="flex flex-col items-start justify-center min-w-full mr-1 ">
-      <div  className="">
-            {DataSidebar.map((item,key)=><Subsidebar key={key} name={item.name} route={item.route} img={item.img}/>)}
+      <div className="">
+        {DataSidebar.map((item, key) => (
+          <div
+            className={`${pathname==item.route?'text-white font-medium bg-hoverbg rounded':'text-gray-400'}`}
+          >
+            <Subsidebar
+              key={key}
+              name={item.name}
+              route={item.route}
+              img={item.img}
+            />
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
-export default Sidebar
+export default Sidebar;
--- Frontend/src/Components/SubsidebarModule.tsx
+++ Frontend/src/Components/SubsidebarModule.tsx
@@ -10,7 +10,7 @@ function SubsidebarModule(props:Props):JSX.Element {
     const { name, img, route } = props;
   return (
     <Link to={route}>
-        <div className="flex  items-center gap-4 min-w-[90%] justify-between my-3 py-1 px-2 cursor-pointer hover:transition transition text-gray-400 hover:font-medium hover:text-white rounded hover:bg-hoverbg ">
+        <div className="flex  items-center gap-4 min-w-[90%] justify-between my-3 py-1 px-2 cursor-pointer hover:transition transition hover:text-white rounded hover:bg-hoverbg ">
           <div className="flex items-center justify-start gap-4">
             <img src={img} className="w-5 h-5" alt="" />
             <div className="col-span-2">{name}</div>
